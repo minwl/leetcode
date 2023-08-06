b6afd80663a20e110da64979f9b5d28ff4c59eaf
minwl <109794693+minwl@users.noreply.github.com>
2023-08-06 13:36:50 +0900

    Time: 23 ms (54.58%), Space: 10 MB (23.04%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -28,6 +28,32 @@ public:
     }
 };
 
+// class Solution {
+// public:
+//     int maxVowels(string s, int k) {
+//         unordered_set<char> vowels{'a', 'e', 'i', 'o', 'u'};
+        
+//         // Build the window of size k, count the number of vowels it contains.
+//         int count = 0;
+//         for (int i = 0; i < k; i++) {
+//             count += vowels.count(s[i]);
+//         } 
+//         int answer = count;
+        
+//         // Slide the window to the right, focus on the added character and the
+//         // removed character and update "count". Record the largest "count".
+//         for (int i = k; i < s.length(); i++) {
+//             count += vowels.count(s[i]) - vowels.count(s[i - k]);
+//             answer = max(answer, count);
+//         }
+        
+//         return answer;
+//     }
+// };
+
+
+
+
 
 //         while(j<n){
         //     if ((s[j]=='a')|(s[j]=='e')|(s[j]=='i')|(s[j]=='o')|(s[j]=='u')){count++;}
