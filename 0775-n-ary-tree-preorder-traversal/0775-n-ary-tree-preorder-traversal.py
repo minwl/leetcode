c3a7b050d74a3c9ebd5bbfc0c69e05b40f6e32f4
minwl <109794693+minwl@users.noreply.github.com>
2023-08-08 13:55:22 +0900

    Time: 50 ms (94.69%), Space: 18.3 MB (89.5%) - LeetHub

---
+++ 0775-n-ary-tree-preorder-traversal/0775-n-ary-tree-preorder-traversal.py
@@ -0,0 +1,15 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if root:
+            ans = [root.val]
+            for child in root.children:
+                ans.extend(self.preorder(child))
+            return ans
